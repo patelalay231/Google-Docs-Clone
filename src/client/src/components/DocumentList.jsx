f2b18d82905ecf524dc8a1b0aa739064bc1e989c
patelalay231 <patelalay231@gmail.com>
2024-08-25 01:56:36 +0530

    pushing

--- src/client/src/components/DocumentList.jsx
+++ src/client/src/components/DocumentList.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { FaFileAlt } from "react-icons/fa"; 
-import { set } from "mongoose";
 const DocumentList = () => {
   const navigate = useNavigate();
   const [documents, setDocuments] = useState([]);
