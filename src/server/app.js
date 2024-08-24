f62ae99303f0366b35315297f26c06ae760e9cd4
patelalay231 <patelalay231@gmail.com>
2024-08-25 00:37:18 +0530

    pushing

--- src/server/app.js
+++ src/server/app.js
@@ -21,9 +21,14 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/',res.send('Welcome to Google Docs Clone API'));
+
 app.use('/api/user', userRoute);
 app.use('/api/document', documentRoute);
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
 app.use(errorHandler);
 
 module.exports = app;
