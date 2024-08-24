994e1af4687599b9f56d93f5734e9dd5a4ce2f61
patelalay231 <patelalay231@gmail.com>
2024-08-24 23:49:38 +0530

    updating app.js

--- src/server/app.js
+++ src/server/app.js
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use('/',res.send('Welcome to Google Docs Clone API'));
 app.use('/api/user', userRoute);
 app.use('/api/document', documentRoute);
 app.use(errorHandler);
